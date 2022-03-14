5c7384468a386ea95a70a87b0f585f0a9d8c587a
Muditha Pradeeptha <99239289+IT21235924@users.noreply.github.com>
2022-03-14 05:07:26 +0000

    meke errors godi

--- Exercise01.cpp
+++ Exercise01.cpp
@@ -5,7 +5,7 @@ int main() {
   
 // == DO NOT CHANGE THE MAIN PROGRAM =================
    Student mystd;
-   mystd.assignDetails(1212, "Amalinga");
+   mystd.assignDetails(6969, "Rajapaksha");
    mystd.display();
 //==========================
    return 0;
